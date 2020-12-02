9981253a02364355c16613475221d8a5a1f33ac7
Mochichi2003 <moyurusuto@gmail.com>
2020-12-02 12:30:29 +0900

    色々追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,12 @@ import Layout from "../components/Layout";
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1 className="text-red-500">Hello Next.js 👋</h1>
+    <h1 className="text-red-400">Hello Next.js 👋</h1>
     <p>
       <Link href="/about">
         <a>About</a>
       </Link>
+      <p>てすとおおおおおおおおおおおおおおお</p>
     </p>
   </Layout>
 );
